719ce7c7168d4bc47ea6bb9ef337dfd6e6de158e
Sin0ra <official.sinovuyoguma@gmail.com>
2026-03-11 06:25:31 +0200

    Update cms-loader.js

--- assets/js/cms-loader.js
+++ assets/js/cms-loader.js
@@ -30,44 +30,29 @@ if (Array.isArray(data.services) && document.getElementById("services-container"
 
   container.innerHTML = data.services.map((service, index) => {
     const id = serviceIds[index] || `service-${index}`;
+    const collapseId = `serviceCollapse${index}`;
 
     return `
       <div class="col-md-6 col-lg-3" id="${id}">
         <div class="service-card p-4 h-100 text-center">
-          
+
           <h5 class="fw-semibold">${service.title || ""}</h5>
           <p>${service.description || ""}</p>
 
           ${service.button_text ? `
-            <button class="btn btn-primary mt-3"
-              data-bs-toggle="modal"
-              data-bs-target="#serviceModal${index}">
+            <button class="btn btn-primary mt-2"
+              data-bs-toggle="collapse"
+              data-bs-target="#${collapseId}">
               ${service.button_text}
             </button>
           ` : ""}
 
-        </div>
-      </div>
-
-      <!-- Modal -->
-      <div class="modal fade" id="serviceModal${index}" tabindex="-1">
-        <div class="modal-dialog modal-dialog-centered">
-          <div class="modal-content">
-
-            <div class="modal-header">
-              <h5 class="modal-title">${service.title}</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-            </div>
-
-            <div class="modal-body">
+          <div class="collapse mt-3" id="${collapseId}">
+            <div class="card card-body">
               ${service.modal_content || ""}
             </div>
-
-            <div class="modal-footer">
-              <button class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            </div>
-
           </div>
+
         </div>
       </div>
     `;
@@ -128,6 +113,7 @@ if (Array.isArray(data.images) && document.getElementById("gallery-container")) 
     console.error("CMS LOAD ERROR:", error);
   }
 }
+
 
 
 
